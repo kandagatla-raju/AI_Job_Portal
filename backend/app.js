6d8e88400e8eb43e99d3441eeffeca036a98b345
Kandagatla Raju <151658582+kandagatla-raju@users.noreply.github.com>
2025-05-18 16:38:07 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://ai-job-portal-0ytr.onrender.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
